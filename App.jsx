25ffb54a6b9a37197ce4fcd5e027702250836753
shermaine <shermaine.lim.si.hui@gmail.com>
2022-05-08 15:19:03 +0800

    add async storage

--- App.jsx
+++ App.jsx
@@ -4,11 +4,42 @@ import { StyleSheet, Text, View } from "react-native";
 import Form from "./components/Form";
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState([]);
   const [editTodo, setEditTodos] = useState(null);
+
+  useEffect(() => {
+    const getDataFromStorage = async () => {
+      const value = await AsyncStorage.getItem("@todos");
+      if (value) {
+        try {
+          const parsedValue = JSON.parse(value);
+          setTodos(parsedValue);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
+
+    getDataFromStorage();
+  }, []);
+
+  useEffect(() => {
+    const setDataToStorage = async () => {
+      try {
+        const stringValue = JSON.stringify(todos);
+        await AsyncStorage.setItem("@todos", stringValue);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    setDataToStorage();
+  }, [todos]);
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
